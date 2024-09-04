dad7606a3ea418b31ed04d1154855c9b6050aba7
Linus <linuscolesharp@gmail.com>
2024-09-04 11:02:25 +0100

    add: added nav

--- my-app/components/Header.jsx
+++ my-app/components/Header.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Link from "next/link";
-
+import { Button } from "./ui/button";
+import Nav from "./Nav";
 
 const Header = () => {
-    return(
-        <header>Header</header>
-    )
-}
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto">
+        <Link href="/">
+          <h1>
+            Linus <span className="text-accent">.</span>
+          </h1>
+        </Link>
+        <Nav />
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
