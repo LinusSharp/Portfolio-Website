90c4e8bccc2643e1584afde835c3b6120a22c248
Linus <linuscolesharp@gmail.com>
2024-09-06 10:22:04 +0100

    feat: list of routes

--- my-app/components/Nav.jsx
+++ my-app/components/Nav.jsx
@@ -3,8 +3,39 @@
 import React from "react";
 import Link from "next/link";
 
+const links = [
+  {
+    name: 'home',
+    path: '/'
+  },
+  {
+    name: 'services',
+    path: '/services'
+  },
+  {
+    name: 'resume',
+    path: '/resume'
+  },
+  {
+    name: 'work',
+    path: '/work'
+  },
+  {
+    name: 'contact',
+    path: '/contact'
+  },
+]
+
 const Nav = () => {
-  return <nav></nav>;
+  return <nav>
+    {links.map((link, index)=> {
+      return (
+        <Link href={link.path} key={index}>
+          {link.name}
+        </Link>
+      )
+    })}
+  </nav>;
 };
 
 export default Nav;
