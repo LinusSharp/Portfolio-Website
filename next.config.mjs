67afff2578e4b0e9f2587c96a48cccad4cd96d4a
Linus <linuscolesharp@gmail.com>
2024-09-10 12:47:16 +0100

    feat: deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true, // Enables React Strict Mode
+  output: "standalone", // Optional: For server-side deployment
+  basePath: "", // Ensure it's empty if you're not using sub-paths
+  trailingSlash: true, // Adds trailing slashes to routes to help with routing issues
+  // You can also enable internationalization (i18n) or other settings if needed
+};
 
 export default nextConfig;
