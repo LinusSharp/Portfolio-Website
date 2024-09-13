506949ac8cf8e501599cd1cef5946371a1b7ebf2
Linus <linuscolesharp@gmail.com>
2024-09-13 14:44:50 +0100

    feat: fixed the mobile navigation

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Sheet, SheetContent, SheetTrigger } from "../components/ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
@@ -30,37 +30,36 @@ const links = [
 ];
 
 const MobileNav = () => {
+  const [isOpen, setIsOpen] = useState(false); 
   const pathname = usePathname();
+
+  const handleLinkClick = () => {
+    setIsOpen(false); 
+  };
+
   return (
-    <Sheet>
-      <SheetTrigger className="flex justify-center items-center">
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
+      <SheetTrigger
+        className="flex justify-center items-center"
+        onClick={() => setIsOpen(!isOpen)}
+      >
         <CiMenuFries className="text-[32px] text-accent" />
       </SheetTrigger>
       <SheetContent className="flex flex-col">
-        {/*logo*/}
-        {/*<div className="mt-32 mb-40 text-center text-2xl">
-          <Link href="/">
-            <h1 className="text-3xl font-semibold">
-              Linus<span className="text-accent">.</span>
-            </h1>
-          </Link>
-        </div>*/}
-        {/*nav*/}
+        {/* nav */}
         <nav className="flex flex-col justify-center items-center gap-8 pt-20">
-          {links.map((link, index) => {
-            return (
-              <Link
-                href={link.path}
-                key={index}
-                className={`${
-                  link.path == pathname &&
-                  "text-accent border-b-2 border-accent"
-                } text-xl capitalize hover:text-accent transition-all`}
-              >
-                {link.name}
-              </Link>
-            );
-          })}
+          {links.map((link, index) => (
+            <Link
+              href={link.path}
+              key={index}
+              onClick={handleLinkClick}
+              className={`${
+                link.path === pathname && "text-accent border-b-2 border-accent"
+              } text-xl capitalize hover:text-accent transition-all`}
+            >
+              {link.name}
+            </Link>
+          ))}
         </nav>
       </SheetContent>
     </Sheet>
